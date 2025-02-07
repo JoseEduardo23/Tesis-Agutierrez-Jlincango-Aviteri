f292e1866411b5434430d12dc18cd5170651af64
Eduardo <joselincango73@gmail.com>
2025-02-06 20:25:36 -0500

    Correccion en las variables de entorno3

--- src/database.js
+++ src/database.js
@@ -1,15 +1,19 @@
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-dotenv.config()
-mongoose.set('strictQuery', true)
+dotenv.config();
+mongoose.set('strictQuery', true);
 
-const connection = async()=>{
-    try {
-        const {connection} = await mongoose.connect(process.env.MONGODB_URL_PRODUCCION)
-        console.log(`Database is connected on ${connection.host} - ${connection.port}`)
-    } catch (error) {
-        console.log(error);
-    }
-}
-export default connection
+const connection = async () => {
+  try {
+    // Verifica si la variable de entorno está cargada correctamente
+    console.log('MongoDB URL:', process.env.MONGODB_URL_PRODUCCION);
+
+    const { connection } = await mongoose.connect(process.env.MONGODB_URL_PRODUCCION);
+    console.log(`Database is connected on ${connection.host} - ${connection.port}`);
+  } catch (error) {
+    console.log('Error connecting to MongoDB:', error);
+  }
+};
+
+export default connection;
