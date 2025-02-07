b56dfedbd1fe820523d7b4872d6311224781c3d5
Eduardo <joselincango73@gmail.com>
2025-02-06 22:11:43 -0500

    Correccion en las variables de entorno5

--- src/server.js
+++ src/server.js
@@ -1,41 +1,47 @@
-// Requerir los módulos
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import adminrouter from './routers/admin_router.js';
 import userrouter from './routers/usuario_router.js';
-import productrouter from './routers/product_router.js'
-import mascotarouter from "./routers/mascota_router.js"
+import productrouter from './routers/product_router.js';
+import mascotarouter from './routers/mascota_router.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 // Inicializaciones
 const app = express();
 dotenv.config();
 
-// Configuraciones 
-app.set('port', process.env.PORT || 3000);
-app.use(cors({
-}));
+// Obtener la ruta del directorio actual
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// Middlewares 
+// Configuraciones
+app.set('port', process.env.PORT || 3000);
+app.use(cors());
 app.use(express.json());
 
-// Rutas 
+// Rutas de la API
+app.use('/api', adminrouter);
+app.use('/api', userrouter);
+app.use('/api', productrouter);
+app.use('/api', mascotarouter);
+
+// Ruta principal
 app.get('/', (req, res) => {
     res.send("Servidor del sistema TIENDANIMAL 🐶🦴🏪🛒");
 });
 
-//Ruta de los administradores
-app.use('/api', adminrouter);
+// Configuración para servir los archivos estáticos de React desde la carpeta "build"
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-//Ruta de los usuarios 
-app.use('/api', userrouter);
+// Ruta para servir el index.html cuando no se encuentra una API o ruta
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
-//Ruta para los productos
-app.use('/api', productrouter)
-
-app.use("/api", mascotarouter)
-
-// Manejo de una ruta que no sea encontrada
+// Manejo de rutas no encontradas (solo para la API)
 app.use((req, res) => res.status(404).send("Endpoint no encontrado - 404"));
 
-// Exportar la instancia de express por medio de app
+// Exportar la instancia de express
 export default app;
