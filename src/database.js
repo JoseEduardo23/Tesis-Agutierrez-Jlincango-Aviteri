d3e372c68cbc83499c32ec7a6b3a359593262f5a
Eduardo <joselincango73@gmail.com>
2025-02-06 20:29:47 -0500

    Correccion en las variables de entorno4

--- src/database.js
+++ src/database.js
@@ -7,9 +7,9 @@ mongoose.set('strictQuery', true);
 const connection = async () => {
   try {
     // Verifica si la variable de entorno está cargada correctamente
-    console.log('MongoDB URL:', process.env.MONGODB_URL_PRODUCCION);
+    console.log('MongoDB URL:', process.env.MONGODB_URL_PRODUCTION);
 
-    const { connection } = await mongoose.connect(process.env.MONGODB_URL_PRODUCCION);
+    const { connection } = await mongoose.connect(process.env.MONGODB_URL_PRODUCTION);
     console.log(`Database is connected on ${connection.host} - ${connection.port}`);
   } catch (error) {
     console.log('Error connecting to MongoDB:', error);
