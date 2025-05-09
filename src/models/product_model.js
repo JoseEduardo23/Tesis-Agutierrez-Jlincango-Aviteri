22208cf81313edd3f100b3d13b60df0d3cf1e03c
Eduardo <joselincango73@gmail.com>
2025-05-09 00:10:55 -0500

    Problemass

--- src/models/product_model.js
+++ src/models/product_model.js
@@ -30,7 +30,7 @@ const productSchema = new Schema({
         type: String,
         required: true,
         trim: true,
-        enum: ['perros', 'gatos', 'aves', 'peces', 'otros'],
+        enum: [],
     }
 }, {
     timestamps: true,
