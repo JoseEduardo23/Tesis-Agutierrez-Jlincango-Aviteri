56149fc5488cf3f1ddea275a0b5119ed9568281f
Eduardo <joselincango73@gmail.com>
2025-05-09 20:55:35 -0500

    Carga de Errores 10

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,10 +1,15 @@
 import { v2 as cloudinary } from 'cloudinary';
-import {config} from 'dotenv';
+import dotenv from 'dotenv';
 
-config();
-cloudinary.vs.config({
+dotenv.config();
+
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true
 });
+
+console.log('Cloudinary configurado correctamente para el cloud:', process.env.CLOUDINARY_CLOUD_NAME);
+
 export default cloudinary;
