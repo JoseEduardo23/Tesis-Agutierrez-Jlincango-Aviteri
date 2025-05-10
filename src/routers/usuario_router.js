5fdbbe38ad02590768948948119ff7b3331418ef
Eduardo <joselincango73@gmail.com>
2025-05-10 18:55:06 -0500

    Carga de Errores 26

--- src/routers/usuario_router.js
+++ src/routers/usuario_router.js
@@ -14,7 +14,7 @@ router.get('/usuario/confirmar/:token', confirmEmail);//🖇
 
 router.get("/usuarios", verificarAutenticacion, listarUsuarios)//🖇 
 router.get('/usuario/perfil', verificarAutenticacion, perfilUsuario); //🖇 
-router.put('/usuario/actualizar-perfil/:id',verificarAutenticacion, uploadUsuarios.single("imagen"),  validacionUsers,actualizarPerfil); //🖇
+router.put('/usuario/actualizar-perfil/:id', verificarAutenticacion, uploadUsuarios.single("imagen"), validacionUsers, actualizarPerfil);
 router.delete("/usuario/eliminar/:id", verificarAutenticacion, eliminarUsuario) //🖇 
 router.put('/usuario/actualizar-password/:id', verificarAutenticacion, actualizarPassword);//
 
