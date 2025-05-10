6107057875725563ec210642644ba5afff66bb70
Eduardo <joselincango73@gmail.com>
2025-05-09 22:54:45 -0500

    Carga de Errores 15

--- src/routers/product_router.js
+++ src/routers/product_router.js
@@ -2,9 +2,12 @@ import { Router } from "express";
 import { registrarProducto, listarProductos, obtenerProductoPorId, actualizarProducto, eliminarProducto, } from "../controllers/product_controller.js";
 import { verificarAutenticacion } from "../helpers/crearJWT.js";
 import { validacionProducto } from "../middlewares/productos_validations.js";
+import uploadErrorHandler from '../middlewares/uploadErrorHandler.js'
+import uploadProductImage from '../config/uploadProductImages.js'
+
 const router = Router();
 
-router.post("/crear/producto", verificarAutenticacion,uploadProductos.single("imagen"),validacionProducto, registrarProducto);
+router.post("/crear/producto", verificarAutenticacion, uploadProductImage, uploadErrorHandler, validacionProducto, registrarProducto);
 router.get("/listar/productos", listarProductos);
 router.get("/detalle/producto/:id", verificarAutenticacion, obtenerProductoPorId);
 router.put("/actualizar/producto/:id", verificarAutenticacion, uploadProductos.single("imagen"), actualizarProducto);
