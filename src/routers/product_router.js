073be755b2636e18fb27c705d9eb6e8c3b465938
Eduardo <joselincango73@gmail.com>
2025-05-09 22:49:22 -0500

    Carga de Errores 14

--- src/routers/product_router.js
+++ src/routers/product_router.js
@@ -2,37 +2,12 @@ import { Router } from "express";
 import { registrarProducto, listarProductos, obtenerProductoPorId, actualizarProducto, eliminarProducto, } from "../controllers/product_controller.js";
 import { verificarAutenticacion } from "../helpers/crearJWT.js";
 import { validacionProducto } from "../middlewares/productos_validations.js";
-import uploadErrorHandler from "../middlewares/uploadErrorHandler.js";
-import uploadProductImage from "../config/uploadProductImages.js";
-
 const router = Router();
 
-// Crear producto (manteniendo ruta original)
-router.post("/crear/producto",
-  verificarAutenticacion,
-  uploadProductImage,
-  uploadErrorHandler,
-  validacionProducto,
-  registrarProducto
-);
-
+router.post("/crear/producto", verificarAutenticacion,uploadProductos.single("imagen"),validacionProducto, registrarProducto);
 router.get("/listar/productos", listarProductos);
-
-router.get("/detalle/producto/:id",
-  obtenerProductoPorId
-);
-
-router.put("/actualizar/producto/:id",
-  verificarAutenticacion,
-  uploadProductImage,
-  uploadErrorHandler,
-  validacionProducto,
-  actualizarProducto
-);
-
-router.delete("/eliminar/producto/:id",
-  verificarAutenticacion,
-  eliminarProducto
-);
+router.get("/detalle/producto/:id", verificarAutenticacion, obtenerProductoPorId);
+router.put("/actualizar/producto/:id", verificarAutenticacion, uploadProductos.single("imagen"), actualizarProducto);
+router.delete("/eliminar/producto/:id", verificarAutenticacion, validacionProducto, eliminarProducto);
 
 export default router;
