0d36026b2fad094adda7116a62fe3c445150d8ef
Eduardo <joselincango73@gmail.com>
2025-05-24 15:49:02 -0500

    Regresar 5

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.set('port', process.env.PORT || 3000);
 
 // Configuración CORS
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173/',
+  origin: process.env.URL_FRONTEND || 'http://localhost:5173/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
