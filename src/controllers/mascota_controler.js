c71fd9545f6be9c7ddda28eb034474a89dcc2df4
AlejandroGutierrez01 <alejandro.gutierrez01@epn.edu.ec>
2025-07-03 23:41:52 -0500

    Validaciones extras para el endpoint de generación de dietas

--- src/controllers/mascota_controler.js
+++ src/controllers/mascota_controler.js
@@ -28,14 +28,14 @@ const registrarMascota = async (req, res) => {
         res.status(201).json({ msg: "Mascota creada con éxito", mascota: nuevaMascota });
     } catch (error) {
         console.log(error);
-        res.status(500).json({ msg: "Error al crear mascota", error:error.message });
+        res.status(500).json({ msg: "Error al crear mascota", error: error.message });
     }
 };
 
 const listarMascotas = async (req, res) => {
     try {
         if (req.UsuarioBDD && req.UsuarioBDD._id) {
-            const mascotas = await Mascota.find({ usuario: req.UsuarioBDD._id }).select("-createdAt -updatedAt -__v").populate("usuario", "_id nombre apellido"); 
+            const mascotas = await Mascota.find({ usuario: req.UsuarioBDD._id }).select("-createdAt -updatedAt -__v").populate("usuario", "_id nombre apellido");
             return res.status(200).json(mascotas);
         } else {
             return res.status(400).json({ msg: "No se ha encontrado un usuario asociado." });
@@ -47,26 +47,26 @@ const listarMascotas = async (req, res) => {
 };
 
 const detalleMascota = async (req, res) => {
-        const { id } = req.params;
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(400).json({ msg: "ID inválido" });
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ msg: "ID inválido" });
+    }
+
+    try {
+        const mascota = await Mascota.findOne({
+            _id: id,
+            usuario: req.UsuarioBDD._id
+        }).populate("usuario", "_id nombre apellido");
+
+        if (!mascota) {
+            return res.status(403).json({ msg: "No tienes permisos para ver esta mascota" });
         }
-    
-        try {
-            const mascota = await Mascota.findOne({ 
-                _id: id, 
-                usuario: req.UsuarioBDD._id
-            }).populate("usuario", "_id nombre apellido");
-    
-            if (!mascota) {
-                return res.status(403).json({ msg: "No tienes permisos para ver esta mascota" });
-            }
-    
-            res.status(200).json(mascota);
-        } catch (error) {
-            res.status(500).json({ msg: "Error al obtener la mascota", error:error.message });
-        }
-    };
+
+        res.status(200).json(mascota);
+    } catch (error) {
+        res.status(500).json({ msg: "Error al obtener la mascota", error: error.message });
+    }
+};
 
 
 const actualizarMascota = async (req, res) => {
@@ -88,7 +88,7 @@ const actualizarMascota = async (req, res) => {
         }
         if (req.file) {
             if (mascota.imagen_id) {
-                 await cloudinary.uploader.destroy(mascota.imagen_id);
+                await cloudinary.uploader.destroy(mascota.imagen_id);
             }
             mascota.imagen = req.file.path;
             mascota.imagen_id = req.file.filename;
@@ -98,7 +98,7 @@ const actualizarMascota = async (req, res) => {
         res.status(200).json({ msg: "Mascota actualizada con éxito", mascota });
     } catch (error) {
         console.log(error);
-        res.status(500).json({ msg: "Error al actualizar mascota", error:error.message });
+        res.status(500).json({ msg: "Error al actualizar mascota", error: error.message });
     }
 };
 
@@ -115,57 +115,74 @@ const eliminarMascota = async (req, res) => {
             return res.status(403).json({ msg: "No tienes permisos para eliminar esta mascota o no se ha encontrado el registro" });
         }
         if (mascota.imagen_id) {
-             await cloudinary.uploader.destroy(mascota.imagen_id);
+            await cloudinary.uploader.destroy(mascota.imagen_id);
         }
-        
+
         await mascota.deleteOne();
         res.status(200).json({ msg: "Mascota eliminada con éxito" });
     } catch (error) {
-        res.status(500).json({ msg: "Error al eliminar mascota", error:error.message });
+        res.status(500).json({ msg: "Error al eliminar mascota", error: error.message });
     }
 };
 
 
 
 const generarDieta = async (req, res) => {
-    
+
     const ai = new GoogleGenAI({ apiKey: `${process.env.GEMINI_API_KEY}` });
 
     const { id } = req.params;
-    
+
     const { presupuesto } = req.body;
+
+    const presupuestosValidos = ["Alto", "Medio", "Bajo"];
+    let presupuestoCategoria = presupuesto;
+
+    const presupuestoEsNumero = !isNaN(presupuesto);
+    const presupuestoNumero = Number(presupuesto);
+
+    if (presupuestoEsNumero && presupuestoNumero >= 1 && presupuestoNumero <= 100) {
+        if (presupuestoNumero <= 33) {
+            presupuestoCategoria = "Bajo";
+        } else if (presupuestoNumero <= 66) {
+            presupuestoCategoria = "Medio";
+        } else {
+            presupuestoCategoria = "Alto";
+        }
+    } else if (!presupuestosValidos.includes(presupuesto)) {
+        return res.status(400).json({ msg: "Ingrese un presupuesto válido" });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(400).json({ msg: "ID inválido" });
+    if (presupuesto === undefined || presupuesto === null) return res.status(400).json({ msg: "Seleccione su presupuesto" });
     
-    const presupuestosValidas = ["Alto", "Medio", "Bajo"];
-
-    if (!presupuestosValidas.includes(presupuesto)) {
-        return res.status(400).json({ msg: "Ingrese un presupuesto valido" });
-    }
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).json({ msg: "ID inválido" });
-    }
-    if (presupuesto === undefined || presupuesto === null) {
-        return res.status(400).json({ msg: "Seleccione su presupuesto" });
-    }
-
     try {
         const mascota = await Mascota.findById(id).findOne({ usuario: req.UsuarioBDD._id });
         if (!mascota) {
             return res.status(403).json({ msg: "No tienes permisos para generar la dieta o no se ha encontrado el registro" });
         }
+        if (mascota.dietaTiempo) {
+            const ahora = new Date();
+            const diferenciaDias = Math.floor((ahora - mascota.dietaTiempo) / (1000 * 60 * 60 * 24));
+
+            if (diferenciaDias < 7) {
+                return res.status(429).json({
+                    msg: `Solo puedes generar una dieta cada 7 días. Faltan ${7 - diferenciaDias} día(s).`
+                });
+            }
+        }
 
         const response = await ai.models.generateContent({
             model: "gemini-2.0-flash",
-            contents: `${process.env.PROMPT_GEMINI} Para una mascota de raza: ${mascota.raza}, edad: ${mascota.edad} años, peso: ${mascota.peso} kg, actividad: ${mascota.actividad} y sin enfermedades conocidas con un presupuesto ${presupuesto} en Ecuador. Se resumido y conciso`
+            contents: `${process.env.PROMPT_GEMINI} Para una mascota de raza: ${mascota.raza}, edad: ${mascota.edad} años, peso: ${mascota.peso} kg, actividad: ${mascota.actividad} y con estas enfermedades: (${mascota.enfermedades}) conocidas con un presupuesto ${presupuestoCategoria} en Ecuador. Se resumido y conciso`
         });
-
         mascota.dieta = response.text;
+        mascota.dietaTiempo = new Date();
         await mascota.save();
-
         res.status(200).json({ msg: "Dieta generada con éxito", dieta: response.text });
     } catch (error) {
         console.log(error);
-        res.status(500).json({ msg: "Error al generar la dieta", error:error.message });
+        res.status(500).json({ msg: "Error al generar la dieta", error: error.message });
     }
 }
 
